3b28e0b8ca4871beb96a783dbf3b79a7e8514e05
vansh deep Arya <92106532+Vansh432@users.noreply.github.com>
2021-12-17 09:47:58 +0530

    Adding Files to Repo

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("welcome to battery mange system");
+let color=document.getElementsByClassName('color');
+let charg=document.getElementById('charg');
+let damage_battery=document.getElementById('damage_battery');
+let damage_bat=document.getElementById('damage_bat');
+let dama_top=document.getElementById('dama_top');
+let gree=document.getElementById('gree');
+console.log(color);
+charg.addEventListener('click',()=>{
+Array.from(color).forEach(Element=>{
+  Element.classList.add('play');
+  damage_battery.style.display="flex";
+})  
+})
+
+
+damage_battery.addEventListener('click',()=>{
+  console.log('dfffff');
+ color[2].classList.add('dama');
+ damage_bat.classList.add('blink');
+ dama_top.classList.add('topblink');
+ damage_bat.style.transform='translateX(-420px)';
+ gree.style.transform="translateX(420px)";
+ 
+})
